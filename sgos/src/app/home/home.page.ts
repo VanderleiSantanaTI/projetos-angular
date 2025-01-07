d39456006e72aec0a33ed32907019e9cc17fbe26
Vanderlei Santana <vanderleisantanati@gmail.com>
2025-01-07 09:27:06 -0300

    feat

--- sgos/src/app/home/home.page.ts
+++ sgos/src/app/home/home.page.ts
@@ -1,3 +1,4 @@
+import { NgClass, NgIf, NgStyle } from '@angular/common';
 import { Component } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
 
@@ -5,8 +6,22 @@ import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/stan
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    NgStyle,
+    NgClass,
+    NgIf
+  ],
 })
 export class HomePage {
+  size : 12 | undefined;
+  font : string = "Arial";
+  classes = ['font', 'size', 'color'];
+  show : boolean = true || null;
+
+
   constructor() {}
 }
