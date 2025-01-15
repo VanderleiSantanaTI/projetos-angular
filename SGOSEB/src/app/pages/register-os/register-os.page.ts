73c44d2d37d30196a7bd97ff2dccc98cd8a125fb
Vanderlei Santana <vanderleisantanati@gmail.com>
2025-01-15 11:58:37 -0300

    fix: melhoria e implantação de formulário no register-os

--- SGOSEB/src/app/pages/register-os/register-os.page.ts
+++ SGOSEB/src/app/pages/register-os/register-os.page.ts
@@ -1,5 +1,6 @@
-import { Component, HostListener} from '@angular/core';
-import { ModalController } from '@ionic/angular';
+import { Component, HostListener, OnInit, QueryList, ViewChildren } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { IonDatetime, ModalController } from '@ionic/angular';
 
 @Component({
   selector: 'app-register-os',
@@ -7,91 +8,75 @@ import { ModalController } from '@ionic/angular';
   styleUrls: ['./register-os.page.scss'],
   standalone: false,
 })
-
-export class RegisterOsPage {
-  marca: string = '';
-  modelo: string = '';
-  placa: string = '';
-  suCia: string = '';
-  patrimonio: number | null = null;
-  hodometro: number | null = null;
-  problema: string = '';
-  sistema: string = '';
-  causa: string = '';
-  manutencao: string = '';
-  dateSelected: string = '';
+export class RegisterOsPage implements OnInit {
+  form: FormGroup;
   isMobile!: boolean;
+  selectedDate: any;
+
+  @ViewChildren(IonDatetime) dateTimeFields!: QueryList<IonDatetime>;
+
+  constructor(private fb: FormBuilder, private modalController: ModalController) {
+    this.form = this.fb.group({
+      marca: ['', Validators.required],
+      modelo: ['', Validators.required],
+      placa: ['', Validators.required],
+      suCia: ['', Validators.required],
+      patrimonio: [null, [Validators.required, Validators.min(1)]],
+      hodometro: [null, [Validators.required, Validators.min(1)]],
+      problema: ['', Validators.required],
+      sistema: ['', Validators.required],
+      causa: ['', Validators.required],
+      manutencao: ['', Validators.required],
+      dateSelected: [null, Validators.required]
+    });
+  }
 
   ngOnInit() {
     this.checkWindowSize();
   }
-  constructor(private modalController: ModalController) {}
 
   async onDateChange(event: any) {
+    this.selectedDate = event.detail.value; // Armazena o valor da data selecionada
+    this.form.controls['dateSelected'].setValue(new Date(this.selectedDate).toLocaleDateString('pt-BR')); // Formata a data e atualiza o formulário
+    // this.form.controls['dateSelected'].setValue(new Date(this.selectedDate))
+    console.log('Data selecionada:', this.form.controls['dateSelected'].value);
 
-    this.dateSelected = event.detail.value; // Armazena o valor da data selecionada
-    this.dateSelected = new Date(this.dateSelected).toLocaleDateString('pt-BR'); // Formata a data
-    console.log('Data selecionada:', this.dateSelected);
-
-       // Fecha o modal após a seleção
+    // Fecha o modal após a seleção
     const modal = await this.modalController.getTop(); // Obtém o modal ativo
     if (modal) {
-         await modal.dismiss(); // Fecha o modal
-       }
+      await modal.dismiss(); // Fecha o modal
+    }
   }
 
+  // clearDate() {
+  //   this.form.controls['dateSelected'].setValue(null);
+  // }
+
   limpar() {
-    this.marca = '';
-    this.modelo = '';
-    this.placa = '';
-    this.suCia = '';
-    this.patrimonio = null;
-    this.hodometro = null;
-    this.problema = '';
-    this.sistema = '';
-    this.manutencao = '';
-    this.causa = '';
-    this.dateSelected = '';
+    this.form.reset();
+    this.dateTimeFields.forEach(date => date.value = '');
   }
 
   cadastrar() {
-    const osData = {
-      marca: this.marca,
-      modelo: this.modelo,
-      placa: this.placa,
-      suCia: this.suCia,
-      patrimonio: this.patrimonio,
-      hodometro: this.hodometro,
-      problema: this.problema,
-      sistema: this.sistema,
-      manutencao: this.manutencao,
-      causa: this.causa,
-      dateSelected: this.dateSelected,
-    };
-    console.log('Cadastro realizado:', osData);
-    alert('Ordem de Serviço cadastrada com sucesso!');
-    window.location.reload();
+    if (this.form.valid) {
+      const osData = this.form.value;
+      console.log('Cadastro realizado:', osData);
+      alert('Ordem de Serviço cadastrada com sucesso!');
+      this.limpar();
+      // window.location.reload();
+    } else {
+      this.adicionarRequired();
+    }
   }
 
-  formValido(): boolean {
-    // Verifica se todos os campos obrigatórios estão preenchidos
-    return (
-      !!this.marca.trim() &&
-      !!this.modelo.trim() &&
-      !!this.placa.trim() &&
-      !!this.suCia.trim() &&
-      this.patrimonio! > 0 &&
-      this.hodometro! > 0 &&
-      !!this.problema.trim() &&
-      !!this.sistema.trim() &&
-      !!this.manutencao.trim() &&
-      !!this.causa.trim() &&
-      !!this.dateSelected
-    );
+  adicionarRequired() {
+    Object.keys(this.form.controls).forEach(key => {
+      this.form.controls[key].setValidators(Validators.required);
+      this.form.controls[key].updateValueAndValidity();
+    });
   }
 
   onKeyPressBlock(event: KeyboardEvent) {
-
     if (isNaN(Number(event.key))) {
       event.preventDefault(); // Bloqueia o caractere
     }
@@ -101,13 +86,13 @@ export class RegisterOsPage {
     const maxLength = 20; // Defina o número máximo de caracteres permitidos
     const input = event.target.value;
     event.target.value = input.slice(0, maxLength); // Limita o número de caracteres
-    if(input.length > maxLength && field === 'hodometro') {
-      this.hodometro = event.target.value; // Atualiza o valor no modelo
-    }else if(input.length > maxLength && field === 'patrimonio') {
-      this.patrimonio = event.target.value; // Atualiza o valor no modelo
-    
-    }
 
+    if (input.length > maxLength && field === 'hodometro') {
+      this.form.controls['hodometro'].setValue(event.target.value); // Atualiza o valor no modelo
+
+    } else if (input.length > maxLength && field === 'patrimonio') {
+      this.form.controls['patrimonio'].setValue(event.target.value); // Atualiza o valor no modelo
+    }
   }
 
   @HostListener('window:resize', ['$event'])
@@ -118,5 +103,4 @@ export class RegisterOsPage {
   checkWindowSize() {
     this.isMobile = window.innerWidth < 768;
   }
-
 }
